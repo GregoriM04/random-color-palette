0da2106588c6643337683089ce749d46fe368bf6
Gregori Martinez <gregorim143@gmail.com>
2024-10-29 10:46:11 -0400

    return to previous palette feature

--- main.js
+++ main.js
@@ -6,9 +6,7 @@ const lockIcons = document.querySelectorAll(".lock-icon");
 const popup = document.getElementById("popup");
 
 // set new color palette after DOM is loaded
-document.addEventListener("DOMContentLoaded", () => {
-  setColors();
-});
+document.addEventListener("DOMContentLoaded", setColors);
 
 // set new color palette when 'spacebar' is pressed
 document.addEventListener("keyup", (event) => {
@@ -17,26 +15,92 @@ document.addEventListener("keyup", (event) => {
   }
 });
 
-// header buttons
+// DOM header buttons
 generateBtn.addEventListener("click", setColors);
 saveBtn.addEventListener("click", getCanvas);
 
-// Function to set colors
+// Store all palette generated so far
+let allGeneratePalette = [];
+let currentColorsArray = [];
+
+// Function to set colors in selected DOM elements
 function setColors() {
+  // reset array to only store current color
+  currentColorsArray = [];
+
   colorContainer.forEach((el) => {
+    // get new color
+    let newColor = getColor();
+    // put new colors to all unlock children
     if (!el.children[0].classList.contains("isLocked")) {
-      let newColor = getColor();
+      // store it on outer array
+      currentColorsArray.push(newColor);
+      // transform new hexcode color based on color brightness
+      let textColor = setContrast(hexToRgb(newColor));
+      // display all colors and set text color
+      el.children[1].style.color = textColor;
+      el.children[0].style.color = textColor;
+      el.children[1].innerText = newColor;
+      el.style.backgroundColor = newColor;
+
+      // maintain the same color is child is locked
+    } else if (el.children[0].classList.contains("isLocked")) {
+      currentColorsArray.push(el.children[1].innerText);
+    }
+  });
+  // saved pallete in history
+  pushStateColorToHistory();
+  return allGeneratePalette.push(currentColorsArray);
+}
+
+// Funttion to remove last palette store in allGeneratePalette
+function removeLastPalette() {
+  let previousPalette;
+  previousPalette = allGeneratePalette.pop();
+
+  return previousPalette;
+}
+
+// Function to access the last palette available in allGeneratePalette
+function getLastPalette() {
+  return allGeneratePalette[allGeneratePalette.length - 1];
+}
+
+// Function to set previous color palette if user go back
+function previousColors() {
+  // remove last palette from display
+  removeLastPalette();
+  // remove all colors saved
+  currentColorsArray = [];
+  // get previous stringPalette
+  let lastColorsPalette = getLastPalette();
+  // save provious colors
+  lastColorsPalette.forEach((el) => currentColorsArray.push(el));
+  // saved pallete in the history
+  pushStateColorToHistory();
+
+  colorContainer.forEach((el) => {
+    let newColor = lastColorsPalette[0]; //"new" color from previous palette
+    if (!el.children[0].classList.contains("isLocked")) {
       let textColor = setContrast(hexToRgb(newColor));
       el.children[1].style.color = textColor;
       el.children[0].style.color = textColor;
       el.children[1].innerText = newColor;
       el.style.backgroundColor = newColor;
+
+      // remove color in index 0 after setting it
+      lastColorsPalette.shift();
+    } else if (el.children[0].classList.contains("isLocked")) {
+      // just remove color in index 0
+      lastColorsPalette.shift();
     }
   });
   return 0;
 }
+// listening for users to trigger history.back() to set previous palette
+window.addEventListener("popstate", previousColors);
 
-// Function to generate ramdon colors
+// Function to generate random colors
 function getColor() {
   let hexLetters = [
     "1",
@@ -112,7 +176,7 @@ lockIcons.forEach((element) => {
   });
 });
 
-// copy hexcode feature
+// Copy hexcode feature
 colorName.forEach((element) => {
   element.addEventListener("click", () => {
     navigator.clipboard.writeText(element.innerText);
@@ -123,19 +187,9 @@ colorName.forEach((element) => {
   });
 });
 
-// Function to get current colors
-function getColorsArray() {
-  let allColorsArray = [];
-  colorContainer.forEach((element) => {
-    allColorsArray.push(element.children[1].innerText);
-  });
-  return allColorsArray;
-}
-
 // Function to create a image file using a canvas element
 function getCanvas() {
-  // get current displayed colors
-  let colorToUse = getColorsArray();
+  // getting colors from currentColorsArray to draw in canvas
   const canvas = document.createElement("canvas");
   document.body.appendChild(canvas);
   canvas.setAttribute("width", "1280px");
@@ -144,51 +198,51 @@ function getCanvas() {
   const ctx = canvas.getContext("2d");
 
   // 1st color
-  ctx.fillStyle = colorToUse[0];
+  ctx.fillStyle = currentColorsArray[0];
   ctx.fillRect(0, 0, 256, 650);
-  ctx.font = "bold 32px Poppins";
+  ctx.font = "normal 32px Poppins";
   ctx.textAlign = "center";
   ctx.textBaseline = "middle";
   ctx.fillStyle = "#ffffff";
   ctx.fillRect(0, 650, 1280, 720);
   ctx.fillStyle = "black";
-  ctx.fillText(colorToUse[0], 120, 690);
+  ctx.fillText(currentColorsArray[0], 120, 690);
 
   // 2nd color
-  ctx.fillStyle = colorToUse[1];
+  ctx.fillStyle = currentColorsArray[1];
   ctx.fillRect(256, 0, 256, 650);
-  ctx.font = "bold 32px Poppins";
+  ctx.font = "normal 32px Poppins";
   ctx.textAlign = "center";
   ctx.textBaseline = "middle";
   ctx.fillStyle = "#000000";
-  ctx.fillText(colorToUse[1], 380, 690);
+  ctx.fillText(currentColorsArray[1], 380, 690);
 
   // 3rd color
-  ctx.fillStyle = colorToUse[2];
+  ctx.fillStyle = currentColorsArray[2];
   ctx.fillRect(512, 0, 256, 650);
-  ctx.font = "bold 32px Poppins";
+  ctx.font = "normal 32px Poppins";
   ctx.textAlign = "center";
   ctx.textBaseline = "middle";
   ctx.fillStyle = "#000000";
-  ctx.fillText(colorToUse[2], 630, 690);
+  ctx.fillText(currentColorsArray[2], 630, 690);
 
   // 4th color
-  ctx.fillStyle = colorToUse[3];
+  ctx.fillStyle = currentColorsArray[3];
   ctx.fillRect(768, 0, 256, 650);
-  ctx.font = "bold 32px Poppins";
+  ctx.font = "normal 32px Poppins";
   ctx.textAlign = "center";
   ctx.textBaseline = "middle";
   ctx.fillStyle = "#000000";
-  ctx.fillText(colorToUse[3], 890, 690);
+  ctx.fillText(currentColorsArray[3], 890, 690);
 
   // 5th color
-  ctx.fillStyle = colorToUse[4];
+  ctx.fillStyle = currentColorsArray[4];
   ctx.fillRect(1024, 0, 256, 650);
-  ctx.font = "bold 32px Poppins";
+  ctx.font = "normal 32px Poppins";
   ctx.textAlign = "center";
   ctx.textBaseline = "middle";
   ctx.fillStyle = "#000000";
-  ctx.fillText(colorToUse[4], 1140, 690);
+  ctx.fillText(currentColorsArray[4], 1140, 690);
 
   // conver canvas to image file
   let dt = canvas.toDataURL("image/png");
@@ -204,4 +258,9 @@ function getCanvas() {
   document.body.removeChild(triggerDownload);
 
   return 0;
+}
+
+// Funtion to add the generated colors to the history without reloading
+function pushStateColorToHistory() {
+  return history.pushState(null, "");
 }
